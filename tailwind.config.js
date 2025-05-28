132458ff7b36a348b5d56d5d38728ff52e8bffb6
thelastroses <Jennifer.Spencer@cuw.edu>
2025-05-27 20:37:22 -0500

    Test

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // Enables dark mode via class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
