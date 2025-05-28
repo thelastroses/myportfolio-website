c5f269621b89697423f5983d402f0b387724cc09
thelastroses <Jennifer.Spencer@cuw.edu>
2025-05-27 20:35:31 -0500

    Arrow Image Color Update

--- app/components/GalleryComponent.jsx
+++ app/components/GalleryComponent.jsx
@@ -62,7 +62,9 @@ const GalleryComponent = () => {
           >
             
       {theme === "dark" ? (
-        <img src="/caret-left.svg" alt="Left" className="w-6 h-6 filter invert brightness-200 transition-all duration-500" style={{ cursor: "pointer" }}/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
+          <path d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753"/>
+        </svg>
       ) : (
         <img src="/caret-left.svg" alt="Left" className="w-6 h-6 transition-all duration-500" style={{ cursor: "pointer" }}/>
       )}
@@ -98,7 +100,9 @@ const GalleryComponent = () => {
             aria-label="Scroll thumbnails right"
           >
             {theme === "dark" ? (
-              <img src="/caret-right.svg" alt="Right" className="w-6 h-6 filter invert brightness-200 transition-all duration-500" style={{ cursor: "pointer" }}/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+  <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
+</svg>
             ) : (
               <img src="/caret-right.svg" alt="Right" className="w-6 h-6 transition-all duration-500" style={{ cursor: "pointer" }}/>
             )}
