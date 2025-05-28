20b6ca6ee25697037b290ec5ec1018e32d78d01f
thelastroses <Jennifer.Spencer@cuw.edu>
2025-05-27 19:29:08 -0500

    Testing code for theme to work

--- app/components/ScrollToTop.js
+++ app/components/ScrollToTop.js
@@ -14,6 +14,8 @@ const ScrollToTop = () => {
         setShowScrollTopButton(false);
       }
     };
+    
+    
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
@@ -24,7 +26,7 @@ const ScrollToTop = () => {
       behavior: "smooth",
     });
   };
-
+  
   if (!showScrollTopButton) return null;
 
   return (
@@ -32,7 +34,9 @@ const ScrollToTop = () => {
       {theme === "dark" ? (
         <img src="/arrow-up.svg" alt="Dark" className="w-6 h-6 transition-all duration-500" />
       ) : (
-        <img src="/arrow-up.svg" alt="Light" className="w-6 h-6 filter invert brightness-200 transition-all duration-500"/> 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 bi bi-arrow-up" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+        </svg>
       )}
     </button>
   );
